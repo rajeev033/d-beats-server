8fd4408f20765bcca810b0b331a32b796d85afe9
Sarthak <sarthakrawatbiz@gmail.com>
2024-03-27 00:46:55 +0530

    Created data models and setting up auth files

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Welcome to D-Beat backend!");
 });
