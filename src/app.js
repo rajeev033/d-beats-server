d0eb336dea729239020fc775fe5e54ca997f4087
Sarthak <sarthakrawatbiz@gmail.com>
2024-03-30 13:57:55 +0530

    Revamped app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const marketplaceAbi = require("./abi/marketplaceAbi.json");
 const routes = require("./routes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
