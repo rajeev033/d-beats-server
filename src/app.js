0e41965b8b348bf7b47f2623b50ffc94a6e2d2f4
Tebbo <leetebbs@ymail.com>
2024-03-30 09:55:56 +0000

    changed exports in app.js

--- src/app.js
+++ src/app.js
@@ -14,19 +14,20 @@ const factoryAddress = "0x242309090561301ff7A27a1d675f2c0dA06845FD";
 const marketplaceAddress = "0x4690C5d846Abb49d0b6B2a04D4aa3B16e4aFC287";
 
 const provider = new ethers.providers.WebSocketProvider(
-  `wss://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_ARB_SEPOLIA_KEY}`
+ `wss://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_ARB_SEPOLIA_KEY}`
 );
 
-module.exports.factoryContract = new ethers.Contract(
-  factoryAddress,
-  factoryAbi,
-  provider
+// Use ES module syntax for exporting
+export const factoryContract = new ethers.Contract(
+ factoryAddress,
+ factoryAbi,
+ provider
 );
 
-module.exports.factoryContract = new ethers.Contract(
-  marketplaceAddress,
-  marketplaceAbi,
-  provider
+export const marketplaceContract = new ethers.Contract(
+ marketplaceAddress,
+ marketplaceAbi,
+ provider
 );
 
 app.use("/api", routes);
@@ -34,9 +35,9 @@ app.use("/api", routes);
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to D-Beat backend!");
+ res.send("Welcome to D-Beat backend!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+ console.log(`Server is running on port ${PORT}`);
 });
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@
 import WeaveDB from "weavedb-sdk-node"
 
 //Initialize WeaveDB
-const db = new WeaveDB({ contractTxId: process.env.CONTRACT_TX_ID });
+const db = new WeaveDB({ contractTxId: "xI5zun1g6UnftxGdwo6ZOy5CXZkpkKMd4Jb6zhf0RHk" });
 await db.init();
 
 export default db;
