6e1d4744ddbf1c8757d291ba95dc73f5661a373a
Tebbo <leetebbs@ymail.com>
2024-03-30 14:24:00 +0000

    changed router exporting

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import marketplaceAbi from "./abi/marketplaceAbi.json" assert { type: "json" };
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+// app.use("api/",routes);
 
 const factoryAddress = "0x242309090561301ff7A27a1d675f2c0dA06845FD";
 const marketplaceAddress = "0x4690C5d846Abb49d0b6B2a04D4aa3B16e4aFC287";
@@ -35,9 +35,9 @@ app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
- res.send("Welcome to D-Beat backend!");
-});
+// app.get("/", (req, res) => {
+//  res.send("Welcome to D-Beat backend!");
+// });
 
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}`);
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,31 @@
+import express from "express";
 import AuthService from "./auth/auth.service.js";
 
+const router = express.Router();
 const authService = new AuthService();
 
-const routes = (app) => {
-  // Register a new user
-  app.post("/api/auth/register", async (req, res) => {
-    try {
-      const { walletAddress } = req.body;
-      const user = await authService.registerUser(walletAddress);
-      res.status(201).json(user);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  });
+router.get("/", (req, res) => {
+ res.send("Welcome to D-Beat backend!");
+});
 
-  // Log in a user
-  app.post("/api/auth/login", async (req, res) => {
-    try {
-      const { walletAddress } = req.body;
-      const token = await authService.loginUser(walletAddress);
-      res.status(200).json({ token });
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  });
-};
+router.post("/auth/register", async (req, res) => {
+ try {
+    const { walletAddress } = req.body;
+    const user = await authService.registerUser(walletAddress);
+    res.status(201).json(user);
+ } catch (err) {
+    res.status(400).json({ error: err.message });
+ }
+});
 
-export default routes;
+router.post("/auth/login", async (req, res) => {
+ try {
+    const { walletAddress } = req.body;
+    const token = await authService.loginUser(walletAddress);
+    res.status(200).json({ token });
+ } catch (err) {
+    res.status(400).json({ error: err.message });
+ }
+});
+
+export default router;
