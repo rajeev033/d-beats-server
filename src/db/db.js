a2d60fc792ddc990e73d591404cf66ba73d39cbb
Sarthak <sarthakrawatbiz@gmail.com>
2024-03-30 22:41:46 +0530

    Made the server run on port 5000 properly

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 import WeaveDB from "weavedb-sdk-node";
 
 //Initialize WeaveDB
-const db = new WeaveDB({ contractTxId: process.env.CONTRACT_TX_ID });
+const db = new WeaveDB({ contractTxId: process.env.REACT_APP_CONTRACT_TX_ID });
 await db.init();
 
 export default db;
